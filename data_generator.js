2aef377057453dc28f14fa23a104fdc1f3bb806e
shawnhr <shawnk723@live.com>
2017-09-23 04:42:22 -0700

    First commit

--- data_generator.js
+++ data_generator.js
@@ -3,6 +3,12 @@
  * You can safely leave this file untouched, and confine your changes to index.html.
  */
 
+ 
+// Show the user new tweets somehow. (You can show them automatically as they're created, or create a button that displays new tweets.
+// Display the timestamps of when the tweets were created. This timestamp should reflect the actual time the tweets were created, and should not just be hardcoded.
+// Design your interface so that you want to look at and use the product you're making.
+// Allow the user to click on a username to see that user's timeline.
+
 // set up data structures
 window.streams = {};
 streams.home = [];
